025de13f5e0d98683552030dc0d5b86ddaf0a76e
kich_alex654@mail.ru <kich_alex654@mail.ru>
2023-02-15 15:06:38 +0300

    React weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [temp, setTemp] = useState('');
+  const [desc, setDesc] = useState('');
+  const [icon, setIcon] = useState('');
+  const [city, setCity] = useState('');
+  const [isReady, setReady] = useState('');
+
+  React.useEffect(() => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=Metric&APIkey=2cf4011d957f2128e1c0340018c61fc6')
+      .then(result => result.json())
+      .then(jsonresult => {
+        setTemp(jsonresult.main.temp);
+        setCity(jsonresult.name);
+        setDesc(jsonresult.weather[0].main);
+        setIcon(jsonresult.weather[0].icon);
+        setReady(true);
+      })
+      .catch(err => console.error(err))
+  }, []);
+
+  if (isReady) {
+    return (
+      <div className="App">
+        <h3>Город: {city}</h3>
+        <p>Температура: {temp} °C</p>
+        <p>Описание: {desc}</p>
+        <img src={'http://openweathermap.org/img/wn/'+icon+'@2x.png'}
+        alt="Weather icon" />
+      </div>
+    );
+  }
+  else {
+    return <div>Loading...</div>
+  }
+
+
 }
 
 export default App;
